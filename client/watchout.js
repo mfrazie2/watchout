6e1e1fd3d68c2dc66c3dcc10d6a81dd9527c7e8c
Mike Frazier <michael.s.frazier@gmail.com>
2016-01-22 19:33:55 -0600

    Added a 'game-board' div to the index page. Added H/R logo as player icon. Implemented code to append board space, asteroids, and player to page. Implemented code to move asteroids around at one second intervals. Implemented code to allow player icon to be draggable.

--- client/watchout.js
+++ client/watchout.js
@@ -1,2 +1,95 @@
 // start slingin' some d3 here.
 
+
+// create a gameboard
+var board = d3.select('.game-board')
+  //append a svg
+  .append('svg')
+  // set a width value
+  .attr('width', 500)
+  //set a height value
+  .attr('height', 500)
+  // set css style background-color
+  .style({'background-color': 'blue'});
+
+// append enemy to gameboard  
+/*var enemy = board.append('image')
+// gets the asteroid image
+  .attr('xlink:href','asteroid.png')
+  // initial position
+  .attr('x',100)
+  .attr('y',100)
+  // size of enemy
+  .attr('height', 100)
+  .attr('width', 100);
+  */
+
+// creates array of 12 enemies with random (x,y) properties  
+var enemies = d3.range(12).map(function(){
+  return {
+    x: Math.random() * 500,
+    y: Math.random() * 500
+  }
+});
+
+for (var i =0 ; i< enemies.length; i ++){
+// iterate through enemies array to append to game board
+  board.append('image')
+  // grabs photo from folder
+    .attr('xlink:href','asteroid.png')
+    // assigns (x,y) positions based on properties of array[i]
+     .attr('x', enemies[i].x)
+     .attr('y', enemies[i].y)
+    // assigns size of asteroid
+    .attr('height', 100)
+    .attr('width', 100)
+    .classed('enemy',true);
+  }
+
+
+
+// drag the hero!
+var drag = d3.behavior.drag().on('drag', function() {
+  hero.attr('x', d3.event.x)
+      .attr('y', d3.event.y);
+});
+
+
+var hero = board.append('image')
+  .attr('xlink:href', 'logo-hr-small.png') 
+  .attr('x',75)
+  .attr('y',250)
+  .attr('height', 100)
+  .attr('width', 100)
+  .call(drag);
+
+
+
+
+
+// move enemies around board to random (x,y)  
+var enemyMove = function() {
+  d3.selectAll('.enemy')
+  .transition()
+  .duration(1000)
+  .attr('x', function() {return Math.random() * 500;})
+  .attr('y', function() {return Math.random() * 500;})
+};
+
+// sets enemies to move every 1000 ms
+setInterval(enemyMove, 1000); 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
