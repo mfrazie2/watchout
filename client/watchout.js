7f082c3b72a060ec930251f4ec1b816554e4641b
Mike Frazier <michael.s.frazier@gmail.com>
2016-01-23 18:26:28 -0600

    Completed implementation of score tallying and collision detection. Updated asteroid enemies to spin on central axis.

--- client/watchout.js
+++ client/watchout.js
@@ -1,7 +1,7 @@
 // Declare global variables
-var highScore,
-    currentScore,
-    numberOfCollisions;
+var highScore = 0,
+    currentScore = 0,
+    collisionCount = 0;
 
 // create a gameboard
 var board = d3.select('.game-board')
@@ -27,26 +27,72 @@ var board = d3.select('.game-board')
   */
 
 // creates array of 12 enemies with random (x,y) properties  
-var enemies = d3.range(12).map(function(){
-  return {
-    x: Math.random() * 500,
-    y: Math.random() * 500
-  }
-});
+var createEnemies = function(n) {
+  return d3.range(0, n).map(function(i){
+    return {
+      id: i,
+      x: Math.random() * 500,
+      y: Math.random() * 500
+    }
+  });
+};
 
-for (var i =0 ; i< enemies.length; i ++){
-// iterate through enemies array to append to game board
-  board.append('image')
-  // grabs photo from folder
-    .attr('xlink:href','asteroid.png')
-    // assigns (x,y) positions based on properties of array[i]
-     .attr('x', enemies[i].x)
-     .attr('y', enemies[i].y)
-    // assigns size of asteroid
-    .attr('height', 100)
-    .attr('width', 100)
-    .classed('enemy',true);
-  }
+/// svg enemies
+d3.select('.game-board')
+  .append('svg')
+  .attr({ id: 'mySVG',
+          width: 100,
+          height: 100 })
+  .append('defs')
+  .attr({ id: 'myDefs'})
+  .append('pattern')
+  .attr({ id: 'image',
+          x: 0,
+          y: 0,
+          height: 50,
+          width: 50 })
+  .append('image')
+  .attr({ x: 0, 
+          y: 0,
+          width: 49,
+          height: 49,
+          'xlink:href': './asteroid.png' });
+  
+  //svg for hero
+  
+  d3.select('.game-board')
+    .append('svg')
+    .attr({id : 'heroSvg', width: 100, height: 100})
+    .append('defs')
+    .attr({id : 'heroDefs'})
+    .append('pattern')
+    .attr({id :'heroImage', x:0, y:0, height: 50, width: 50})
+    .append('image')
+    .attr({x:0 ,y:0, height: 49, width: 49, 'xlink:href': './logo-hr-small.png' });
+    
+
+var enemies = board.selectAll('circle')
+  .data(createEnemies(12))
+  .enter()
+  .append('circle')
+  .attr('class', 'enemy')
+  .attr('cx', function(d){return d.x})
+  .attr('cy', function(d){return d.y})
+  .attr('r', 20)
+  .style('fill', 'url(#image)');
+// for (var i =0 ; i< enemies.length; i ++){
+// // iterate through enemies array to append to game board
+//   board.append('image')
+//   // grabs photo from folder
+//     .attr('xlink:href','asteroid.png')
+//     // assigns (x,y) positions based on properties of array[i]
+//      .attr('x', enemies[i].x)
+//      .attr('y', enemies[i].y)
+//     // assigns size of asteroid
+//     .attr('height', 100)
+//     .attr('width', 100)
+//     .classed('enemy',true);
+//   }
 
 
 
@@ -70,44 +116,98 @@ var drag = d3.behavior.drag().on('drag', function() {
 //   .attr('height', 100)
 //   .attr('width', 100)
   
-  
-var hero = board.append('circle')
-  .attr('cx', 50)
-  .attr('cy', 50)
-  .attr('r', 50)
-  .call(drag);
+ var hero = board.append('circle')
+  .attr('class', 'hero')
+  .attr('cx', 250)
+  .attr('cy', 250)
+  .attr('r', 25)
+  .style('fill', 'url(#heroImage)')
+  .call(drag)
+// var hero = board.append('circle')
+//   .attr('cx', 50)
+//   .attr('cy', 50)
+//   .attr('r', 50)
+//   .call(drag);
 
-board.selectAll('circle')
-  .append('image')
-  .attr('xlink:href', 'logo-hr-small.png')
-  .attr('height', 25)
-  .attr('width', 25);
+// board.selectAll('circle')
+//   .append('image')
+//   .attr('xlink:href', 'logo-hr-small.png')
+//   .attr('height', 25)
+//   .attr('width', 25);
 
 
 
 // move enemies around board to random (x,y)  
 var enemyMove = function() {
-  d3.selectAll('.enemy')
+  board.selectAll('.enemy')
   .transition()
   .duration(1000)
-  .attr('x', function() {return Math.random() * 500;})
-  .attr('y', function() {return Math.random() * 500;})
+  .tween('collisionDetector', detectCollisions)
+  .attr('cx', function() {return Math.random() * 500;})
+  .attr('cy', function() {return Math.random() * 500;})
+  
 };
 
-var detectCollisions = function(enemy) {
-  d3.select('.collisions span')
-    .text(numberOfCollisions);
-  // calculate distance between hero and enemy
+var detectCollisions = function() {
+  var hasCollided = false;
+  var that = d3.select(this);
+  return function(){
+    //console.log("Working");
+    if (!hasCollided){
+      var currentX = Math.floor(that.attr('cx'));
+      var currentY = Math.floor(that.attr('cy'));
+      var currentR = that.attr('r');
+      var heroY = hero.attr('cy');
+      var heroX = hero.attr('cx');
+      var heroR = hero.attr('r');
+  
+    // use pythag to get distance between hero and enemy
+      var distance = Math.sqrt(Math.pow(heroX - currentX, 2) + Math.pow(heroY - currentY, 2));
+    //console.log(distance);
+    // if distance is less than the hero radius
+        if(distance < 1.5* heroR) {
+          console.log("Collision!")
+          hasCollided = true;
+          updateCollisionCount();
+          updateHighScore();
+          currentScore = 0;
+          
+      //collisions
+        }
+   }
+  };
+  // d3.select('.collisions span')
+  //   .text(numberOfCollisions);
+  // // calculate distance between hero and enemy
   // if distance < XX
     // collision!
     // collision count increment
 };
 
-//detectCollisions();
+ var updateCollisionCount =function(){
+   collisionCount++;
+   d3.select('.collisions span')
+    .text(collisionCount);
+ }
 
+var updateScore = function() {
+  currentScore++;
+  d3.select('.current span')
+    .text(currentScore);
+};
 
+var updateHighScore = function() {
+  if(currentScore > highScore) {
+    highScore = currentScore;
+  }
+  d3.select('.high span')
+      .text(highScore);
+};
+
+// update the current score every 250 ms
+setInterval(updateScore, 250);
 // sets enemies to move every 1000 ms
-//setInterval(enemyMove, 1000); 
+ setInterval(enemyMove, 1000); 
 
 
 
