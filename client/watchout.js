8213efa75852f8960de49431bc23d8fb0136ddea
Mike Frazier <michael.s.frazier@gmail.com>
2016-01-23 19:29:40 -0600

    Added background image for the game board. Refactored the function to move asteroids around the game board.

--- client/watchout.js
+++ client/watchout.js
@@ -3,74 +3,54 @@ var highScore = 0,
     currentScore = 0,
     collisionCount = 0;
 
+var windowHeight = window.innerHeight,
+    windowWidth = window.innerWidth;
+
 // create a gameboard
 var board = d3.select('.game-board')
   //append a svg
   .append('svg')
   // set a width value
-  .attr('width', 500)
+  .attr('width', windowWidth)
   //set a height value
-  .attr('height', 500)
+  .attr('height', windowHeight)
   // set css style background-color
-  .style({'background-color': 'blue'});
-
-// append enemy to gameboard  
-/*var enemy = board.append('image')
-// gets the asteroid image
-  .attr('xlink:href','asteroid.png')
-  // initial position
-  .attr('x',100)
-  .attr('y',100)
-  // size of enemy
-  .attr('height', 100)
-  .attr('width', 100);
-  */
+  .style({'background-image': 'url(./background.png)' });
 
 // creates array of 12 enemies with random (x,y) properties  
 var createEnemies = function(n) {
   return d3.range(0, n).map(function(i){
     return {
       id: i,
-      x: Math.random() * 500,
-      y: Math.random() * 500
+      x: Math.random() * windowWidth,
+      y: Math.random() * windowHeight
     }
   });
 };
 
-/// svg enemies
+// svg enemies
 d3.select('.game-board')
   .append('svg')
-  .attr({ id: 'mySVG',
-          width: 100,
-          height: 100 })
+  .attr({ id: 'mySVG', width: 100, height: 100 })
   .append('defs')
   .attr({ id: 'myDefs'})
   .append('pattern')
-  .attr({ id: 'image',
-          x: 0,
-          y: 0,
-          height: 50,
-          width: 50 })
+  .attr({ id: 'image', x: 0, y: 0, height: 50, width: 50 })
   .append('image')
-  .attr({ x: 0, 
-          y: 0,
-          width: 49,
-          height: 49,
-          'xlink:href': './asteroid.png' });
+  .attr({ x: 0, y: 0, width: 49, height: 49, 'xlink:href': './asteroid.png' });
   
-  //svg for hero
-  
-  d3.select('.game-board')
-    .append('svg')
-    .attr({id : 'heroSvg', width: 100, height: 100})
-    .append('defs')
-    .attr({id : 'heroDefs'})
-    .append('pattern')
-    .attr({id :'heroImage', x:0, y:0, height: 50, width: 50})
-    .append('image')
-    .attr({x:0 ,y:0, height: 49, width: 49, 'xlink:href': './logo-hr-small.png' });
+// svg for hero
+d3.select('.game-board')
+  .append('svg')
+  .attr({id : 'heroSvg', width: 100, height: 100})
+  .append('defs')
+  .attr({id : 'heroDefs'})
+  .append('pattern')
+  .attr({id :'heroImage', x:0, y:0, height: 50, width: 50})
+  .append('image')
+  .attr({x:0 ,y:0, height: 49, width: 49, 'xlink:href': './logo-hr-small.png' });
     
-
+// add enemies to the game board
 var enemies = board.selectAll('circle')
   .data(createEnemies(12))
   .enter()
@@ -80,74 +60,34 @@ var enemies = board.selectAll('circle')
   .attr('cy', function(d){return d.y})
   .attr('r', 20)
   .style('fill', 'url(#image)');
-// for (var i =0 ; i< enemies.length; i ++){
-// // iterate through enemies array to append to game board
-//   board.append('image')
-//   // grabs photo from folder
-//     .attr('xlink:href','asteroid.png')
-//     // assigns (x,y) positions based on properties of array[i]
-//      .attr('x', enemies[i].x)
-//      .attr('y', enemies[i].y)
-//     // assigns size of asteroid
-//     .attr('height', 100)
-//     .attr('width', 100)
-//     .classed('enemy',true);
-//   }
-
-
 
 // drag the hero!
-var drag = d3.behavior.drag().on('drag', function() {
-  hero.attr('cx', d3.event.x)
-      .attr('cy', d3.event.y);
-});
+var drag = d3.behavior
+  .drag().on('dragstart', function() {circle.style('fill', 'url(#heroImage')})
+  .on('drag', function() {hero.attr('cx', d3.event.x).attr('cy', d3.event.y);})
+  .on('dragend', function() {circle.style('fill', 'url(#heroImage')});
 
-
-// var defs = board.append('svg:defs')
-//   .append('svg:pattern')
-//   .classed('hero', true)
-//   .attr('patternUnits', 'userSpaceOnUse')
-//   .attr('height', 100)
-//   .attr('width', 100)
-//   .append('svg:image')
-//   .attr('xlink:href', 'logo-hr-small.png') 
-//   .attr('x',75)
-//   .attr('y',250)
-//   .attr('height', 100)
-//   .attr('width', 100)
-  
+// add hero to game board  
  var hero = board.append('circle')
   .attr('class', 'hero')
   .attr('cx', 250)
   .attr('cy', 250)
   .attr('r', 25)
   .style('fill', 'url(#heroImage)')
-  .call(drag)
-// var hero = board.append('circle')
-//   .attr('cx', 50)
-//   .attr('cy', 50)
-//   .attr('r', 50)
-//   .call(drag);
-
-// board.selectAll('circle')
-//   .append('image')
-//   .attr('xlink:href', 'logo-hr-small.png')
-//   .attr('height', 25)
-//   .attr('width', 25);
-
-
+  .call(drag);
 
 // move enemies around board to random (x,y)  
 var enemyMove = function() {
-  board.selectAll('.enemy')
+  //board.selectAll('.enemy')
+   enemies.data(createEnemies(12))
   .transition()
-  .duration(1000)
+  .duration(500)
   .tween('collisionDetector', detectCollisions)
-  .attr('cx', function() {return Math.random() * 500;})
-  .attr('cy', function() {return Math.random() * 500;})
-  
+  .attr('cx', function(d) {return d.x;})
+  .attr('cy', function(d) {return d.y;});
 };
 
+// detect collisions between enemies and hero during transition
 var detectCollisions = function() {
   var hasCollided = false;
   var that = d3.select(this);
@@ -160,42 +100,34 @@ var detectCollisions = function() {
       var heroY = hero.attr('cy');
       var heroX = hero.attr('cx');
       var heroR = hero.attr('r');
-  
     // use pythag to get distance between hero and enemy
       var distance = Math.sqrt(Math.pow(heroX - currentX, 2) + Math.pow(heroY - currentY, 2));
-    //console.log(distance);
-    // if distance is less than the hero radius
-        if(distance < 1.5* heroR) {
-          console.log("Collision!")
-          hasCollided = true;
-          updateCollisionCount();
-          updateHighScore();
-          currentScore = 0;
-          
-      //collisions
-        }
-   }
+    // if distance is less than 1.5 x hero radius
+      if(distance < 1.5* heroR) {
+        hasCollided = true;
+        updateCollisionCount();
+        updateHighScore();
+        currentScore = 0;
+      }
+    }
   };
-  // d3.select('.collisions span')
-  //   .text(numberOfCollisions);
-  // // calculate distance between hero and enemy
-  // if distance < XX
-    // collision!
-    // collision count increment
 };
 
- var updateCollisionCount =function(){
-   collisionCount++;
-   d3.select('.collisions span')
+// function to update the collision count and append it on the scoreboard 
+var updateCollisionCount =function(){
+  collisionCount++;
+  d3.select('.collisions span')
     .text(collisionCount);
- }
+}
 
+// function to update the current score and append it on the scoreboard
 var updateScore = function() {
   currentScore++;
   d3.select('.current span')
     .text(currentScore);
 };
 
+// function to update the high score, if needed, and append it on the scoreboard
 var updateHighScore = function() {
   if(currentScore > highScore) {
     highScore = currentScore;
@@ -206,20 +138,6 @@ var updateHighScore = function() {
 
 // update the current score every 250 ms
 setInterval(updateScore, 250);
+
 // sets enemies to move every 1000 ms
  setInterval(enemyMove, 1000); 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
